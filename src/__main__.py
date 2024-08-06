681c5a733db2c416512bb268cab013e1c55ee995
Yurin Andrey <andreyyurin@icloud.com>
2024-08-06 18:26:00 +0300

    Инициализация

---
+++ src/__main__.py
@@ -0,0 +1,16 @@
+from loguru import logger
+
+
+def main():
+    """Main method. Entry point."""
+    try:
+        ...
+    except Exception as ex:
+        logger.critical('You have done something wrong! {0}'.format(str(ex)))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.critical('Shutting down, bye!')
