0839bcc3e5ba07538ce82baee3f07d01ed5e0090
Yurin Andrey <andreyyurin@icloud.com>
2024-08-08 21:49:09 +0300

    Удалил строку, использовавшуюся для дебага

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,6 @@ from src.configs.logger_config import logger
 def main():
     """Main method. Entry point."""
     try:
-        a = 10/0
         ...  # noqa: WPS428
     except Exception as ex:
         logger.opt(exception=True).critical('You have done something wrong! {0}'.format(str(ex)))
