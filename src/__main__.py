9d7c4e8f39d93267967ed4fd658119bdd28e668c
Yurin Andrey <andreyyurin@icloud.com>
2024-08-08 21:45:35 +0300

    Написал logger config
    Также:
    - внёс связанные с этим изменения
    - изменил файловую структуру

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,17 @@
-from loguru import logger
+from src.configs.logger_config import logger
 
 
 def main():
     """Main method. Entry point."""
     try:
+        a = 10/0
         ...  # noqa: WPS428
     except Exception as ex:
-        logger.critical('You have done something wrong! {0}'.format(str(ex)))
+        logger.opt(exception=True).critical('You have done something wrong! {0}'.format(str(ex)))
 
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        logger.critical('Shutting down, bye!')
+        logger.opt(exception=True).critical('Shutting down, bye!')
