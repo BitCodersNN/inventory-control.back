1ae17ff8383494b5b6d3128f2975b908124a16fd
Stepa <56769104+Stepa123de@users.noreply.github.com>
2024-08-14 18:20:23 +0300

    Создан пример FastApi для auth (#3)

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,13 @@
+import uvicorn
 from loguru import logger
+
+from src.config import SERVER_PORT
 
 
 def main():
     """Main method. Entry point."""
     try:
-        ...  # noqa: WPS428
+        uvicorn.run('src.api:app', port=SERVER_PORT, reload=True)
     except Exception as ex:
         logger.critical('You have done something wrong! {0}'.format(str(ex)))
 
