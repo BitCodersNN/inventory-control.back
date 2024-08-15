a21cd70bf2ab2862a51003eaa6ad150d1c7fa50b
Yurin Andrey <andreyyurin@icloud.com>
2024-08-15 19:38:14 +0300

    Исправления

--- src/configs/logger/logger_config.py
+++ src/configs/logger/logger_config.py
@@ -6,17 +6,13 @@ from loguru import logger
 from src.configs.logger.console_logger import CONSOLE_HANDLERS
 from src.configs.logger.loki_logger import LOKI_LOGGER_HANDLER
 
-
-def _get_handlers(mode):
-    match mode:
-        case 'Debug':
-            return CONSOLE_HANDLERS
-        case 'Release':
-            return LOKI_LOGGER_HANDLER
-
+handlers: Final = {
+    'Debug': CONSOLE_HANDLERS,
+    'Release': CONSOLE_HANDLERS + LOKI_LOGGER_HANDLER,
+}
 
 _MODE: Final = os.getenv('MODE', 'Debug')
 
 logger.configure(
-    handlers=_get_handlers(_MODE),
+    handlers=handlers.get(_MODE, CONSOLE_HANDLERS),
 )
