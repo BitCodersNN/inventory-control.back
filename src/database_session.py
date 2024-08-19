002e3dfa3df6c2b58cec8d485e35e1ac4f5a5000
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2024-08-19 20:01:26 +0300

    Добавление декоратора из модуля contextlib и try-except  (#7)

--- src/database_session.py
+++ src/database_session.py
@@ -1,23 +1,30 @@
+import contextlib
 from typing import AsyncGenerator, Final
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy import NullPool
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
 
 from src.configs.db_config import ASYNC_POSTGRES_URL
+from src.configs.logger.logger_config import logger
 
 BASE: Final = declarative_base()
 
 ENGINE: Final = create_async_engine(ASYNC_POSTGRES_URL, poolclass=NullPool)
 
-ASYNC_SESSION_MAKER: Final = sessionmaker(
-    engine=ENGINE,
+ASYNC_SESSION_MAKER: Final = async_sessionmaker(
+    ENGINE,
     class_=AsyncSession,
     expire_on_commit=False,
 )
 
 
+@contextlib.asynccontextmanager
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     """
     Asynchronously yield an SQLAlchemy AsyncSession.
@@ -25,5 +32,13 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     Yields:
         AsyncSession: An asynchronous SQLAlchemy session.
     """
-    async with ASYNC_SESSION_MAKER() as session:
-        yield session
+    try:
+        async with ASYNC_SESSION_MAKER() as session:
+            yield session
+    except SQLAlchemyError as ex:
+        logger.exception(
+            'Не удалось создать сессию с БД! \nSQLAlchemy error: {0}'.format(
+                str(ex),
+            ),
+        )
+        raise
