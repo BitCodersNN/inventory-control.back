eb3a01afee18769f508b070075b7e51abc8e3bb9
Yurin Andrey <andreyyurin@icloud.com>
2024-08-19 19:42:48 +0300

    Рефакторинг

--- src/database_session.py
+++ src/database_session.py
@@ -1,6 +1,10 @@
 from typing import AsyncGenerator, Final
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import NullPool
 
