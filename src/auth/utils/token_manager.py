d5b4bbb2c69cdc03fc1b5f67720e941beac793cd
Stepa123de <stepa.kashin.2018@gmail.com>
2024-09-08 15:17:29 +0300

    fix

--- src/auth/utils/token_manager.py
+++ src/auth/utils/token_manager.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from typing import Optional
 from uuid import UUID, uuid4
 
 import jwt
@@ -26,29 +27,21 @@ class TokenManager:
 
     refresh: Обновляет токен доступа, используя токен обновления.
 
-    _create_access_token: Создает новый токен доступа на основе
-                          данных пользователя.
-
-    _create_refresh_token: Создает новый токен обновления и
-                           сохраняет его в базе данных.
-
-    validate_token: Проверяет действительность токена доступа.
+    decode_token: Проверяет действительность токена доступа.
     """
 
     @classmethod
     async def create_tokens(
         cls,
         session: AsyncSession,
-        user_id: UUID,
-        role: UserRoles,
+        user: UserModel,
     ) -> Token:
         """
         Создаёт токены доступа и обновления для пользователя.
 
         Args:
             session (AsyncSession): Сессия базы данных.
-            user_id (UUID): Идентификатор пользователя.
-            role (UserRoles): Роль пользователя.
+            user (UserModel): Пользователь.
 
         Returns:
             Token: Объект с током доступа и обновления.
@@ -56,26 +49,25 @@ class TokenManager:
         Raises:
             ValueError: Если пользователь не найден.
         """
-        user: UserModel = await UserDAO.find_one_or_none(
-            session,
-            user_id=user_id,
-            role=role,
-        )
-        if not user:
-            raise ValueError('Пользователь не найден.')
 
         access_token: str = await cls._create_access_token(session, user)
         refresh_token: UUID = await cls._create_refresh_token(session, user)
         return Token(access_token=access_token, refresh_token=refresh_token)
 
     @classmethod
-    async def refresh(cls, session: AsyncSession, refresh_token: UUID) -> Token:
+    async def refresh(
+        cls,
+        session: AsyncSession,
+        old_refresh_token: RefreshTokenModel,
+        user: UserModel,
+    ) -> Token:
         """
         Обновляет токены доступа и обновления.
 
         Args:
             session (AsyncSession): Сессия базы данных.
-            refresh_token (UUID): Токен обновления.
+            old_refresh_token (RefreshTokenModel): Токен обновления.
+            user (UserModel): Пользователь.
 
         Returns:
             Token: Новый токен доступа и обновления.
@@ -83,25 +75,13 @@ class TokenManager:
         Raises:
             ValueError: Если токен не найден или не устарел.
         """
-        old_refresh_token: RefreshTokenModel = await (
-            RefreshTokenDAO.find_one_or_none(
-                session,
-                refresh_token=refresh_token,
-            ),
-        )
-        user: UserModel = await UserDAO.find_one_or_none(
-            session,
-            user_id=old_refresh_token.user_id,
-        )
-        if not old_refresh_token or not user:
-            raise ValueError('Пользователь или Токен не найден.')
+        if old_refresh_token.user_id != user.user_id:
+            raise ValueError('user_id != user_id')
 
-        created_at: datetime = datetime.fromisoformat(
-            old_refresh_token.created_at,
-        )
+        created_at: datetime = old_refresh_token.created_at
         expires_in: int = int(old_refresh_token.expires_in)
-        date_end = created_at + timedelta(seconds=expires_in)
-        if date_end < datetime.now():
+        date_end: datetime = created_at + timedelta(seconds=expires_in)
+        if date_end < datetime.now(timezone.utc):
             raise ValueError('Рефреш токен устарел.')
 
         RefreshTokenDAO.update(
@@ -116,7 +96,7 @@ class TokenManager:
         )
 
     @classmethod
-    async def validate_token(cls, access_token: str) -> dict:
+    async def decode_token(cls, access_token: str) -> Optional[dict]:
         """
         Проверяет корректность токена доступа.
 
@@ -133,35 +113,32 @@ class TokenManager:
                 algorithms=[TOKEN_ALG],
             )
         except jwt.ExpiredSignatureError:
-            return {}
+            return None
         except jwt.InvalidTokenError:
-            return {}  # Здесь можно добавить логику обработки исключения.
+            return None  # Здесь можно добавить логику обработки исключения.
 
         return decoded_payload
 
     @classmethod
     async def _create_access_token(
         cls,
-        session: AsyncSession,
         user: UserModel,
     ) -> str:
         """
         Создаёт токен доступа для пользователя.
 
         Args:
-            session (AsyncSession): Сессия базы данных.
             user (UserModel): Модель пользователя.
 
         Returns:
             str: Токен доступа.
         """
-        created_at = datetime.now()
+        created_at: datetime = datetime.now(timezone.utc)
         exp = timedelta(seconds=ACCESS_TOKEN_EXPIRE_SECONDS)
         exp += created_at.utcnow()
         token_data: dict = {
-            'user_id': user.user_id,
-            'role': user.role,
-            'created_at': created_at,
+            'sub': user.user_id,
+            'iat': created_at,
             'exp': exp,
         }
         return jwt.encode(
