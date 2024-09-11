b94744c30a311ae81608797eebdaaa28ae77e8bc
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2024-09-11 20:54:50 +0300

    Переименовал RefreshToken в RefreshSession и также убрал работу с БД (#23)

---
+++ src/auth/dao/refresh_session.py
@@ -0,0 +1,23 @@
+from src.auth.models import RefreshSessionModel
+from src.auth.schemas.refresh_session import (
+    RefreshSessionCreate,
+    RefreshSessionUpdate,
+)
+from src.utils.base_dao import BaseDAO
+
+
+class RefreshSessionDAO(
+    BaseDAO[RefreshSessionModel, RefreshSessionCreate, RefreshSessionUpdate],
+):
+    """
+    Объект доступа к данным для модели RefreshSessionModel.
+
+    Этот класс наследуется от BaseDAO и предоставляет методы для выполнения
+    операций CRUD (создание, чтение, обновление, удаление)
+    над объектами модели RefreshSessionModel.
+
+    Attributes:
+        model (RefreshSessionModel): Модель данных, с которой работает DAO.
+    """
+
+    model = RefreshSessionModel
