c87f8f559863467265e9817fa14a280b3d77d34c
Stepa123de <stepa.kashin.2018@gmail.com>
2024-09-20 11:00:40 +0300

    create routs

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,13 +1,88 @@
-from fastapi import APIRouter, Response
-from starlette.status import HTTP_200_OK  # noqa: F401
+from uuid import UUID
 
-from src.auth import schemas  # noqa: F401
-from src.auth.models import users  # noqa: F401
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.auth.auth_service import AuthService
+from src.auth.schemas.token import Token
+from src.auth.schemas.user import UserAuth
 
 router = APIRouter()
+auth_service = AuthService()
+async_session = AsyncSession()
+oauth2_scheme = Depends(OAuth2PasswordBearer(tokenUrl='token'))
 
 
-@router.get('/echo/{string}')
-def set_task(string: str, response: Response) -> str:
-    """Example."""
-    return string[::-1]
+@router.get('/authenticate')
+def authenticate(
+    auth_user: UserAuth,
+):
+    """
+    Аутентифицирует пользователя.
+
+    Args:
+        auth_user: Данные пользователя для аутентификации.
+
+    Returns:
+        Результат аутентификации.
+    """
+    return auth_service.authenticate(async_session, auth_user)
+
+
+@router.get('/logout')
+@auth_service.identification
+def logout(
+    refresh_token: UUID,
+    access_token: str = oauth2_scheme,
+):
+    """
+    Выход пользователя из системы.
+
+    Args:
+        refresh_token: Refresh token пользователя.
+        access_token: Access token пользователя.
+
+    Returns:
+        Результат выхода из системы.
+    """
+    token: Token = Token(access_token=access_token, refresh_token=refresh_token)
+    return auth_service.logout(async_session, token)
+
+
+@router.get('/logout_from_all_devices')
+def logout_from_all_devices(
+    refresh_token: UUID,
+    access_token: str = oauth2_scheme,
+):
+    """
+    Выход пользователя из системы со всех устройств.
+
+    Args:
+        refresh_token: Refresh token пользователя.
+        access_token: Access token пользователя.
+
+    Returns:
+        Результат выхода из системы со всех устройств.
+    """
+    token: Token = Token(access_token=access_token, refresh_token=refresh_token)
+    return auth_service.logout_from_all_devices(async_session, token)
+
+
+@router.get('/refresh')
+def refresh(
+    refresh_token: UUID,
+    access_token: str = oauth2_scheme,
+):
+    """
+    Обновляет access token пользователя.
+
+    Args:
+        refresh_token: Refresh token пользователя.
+        access_token: Access token пользователя.
+
+    Returns:
+        Новый access token.
+    """
+    token: Token = Token(access_token=access_token, refresh_token=refresh_token)
+    return auth_service.refresh(async_session, token)
